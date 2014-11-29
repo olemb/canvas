d82a3d24302dacf2950a2a0ef91ec1c5d6642584
Ole Martin Bjørndalen <ombdalen@gmail.com>
2014-11-29 15:42:34 +0100

    Added get_block_size/_nframes() and _pa_init() (called when port is opened).

--- audio.py
+++ audio.py
@@ -2,14 +2,27 @@ import wave
 import pyaudio
 from pyaudio import PyAudio
 
-pa = PyAudio()
+def get_block_size():
+    """Return block size in bytes."""
+    return 1024
+
+def get_block_nframes():
+    """Return block size in frames."""
+    return 256
+
+def _pa_init(self):
+    global pa
+    if pa is None:
+        pa = PyAudio()
 
 AUDIO_FORMAT = dict(format=pyaudio.paInt16, channels=2, rate=44100)
 
 def open_input():
+    _pa_init()
     return pa.open(input=True, **AUDIO_FORMAT)
 
 def open_output():
+    _pa_init()
     return pa.open(output=True, **AUDIO_FORMAT)
 
 def open_wavefile(filename, mode):
