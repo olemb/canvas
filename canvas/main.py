1134017341b96cc7f76502887d35bc634cf60833
Ole Martin Bjørndalen <ombdalen@gmail.com>
2017-01-11 13:09:18 +0100

    Now creates a new canvas on the desktop if started without a dirname.

--- canvas/main.py
+++ canvas/main.py
@@ -1,3 +1,5 @@
+import os
+import time
 import click
 from .gui import GUI
 from .transport import Transport
@@ -8,11 +10,22 @@ CLIPS = [
     # Clip('', start=0.5, length=0, y=0.55, load=False),
 ]
 
+def get_dirname():
+    return os.path.expanduser('~/Desktop/canvas-{:d}'.format(int(time.time())))
+
+
 @click.command()
 @click.option('--mix', default=None, help='Mix down recording to wav file.',
               type=click.Path())
-@click.argument('dirname', type=click.Path())
+@click.argument('dirname', type=click.Path(), nargs=-1)
 def main(mix, dirname):
+    if len(dirname) == 0:
+        dirname = get_dirname()
+    elif len(dirname) == 1:
+        dirname = dirname[0]
+    else:
+        raise click.ArguementError('too many arguments')
+
     if mix:
         transport = Transport(dirname)
         transport.load()
