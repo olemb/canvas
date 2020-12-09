b1cb31121ccc9d6b3c2ef5ff33afef8a2d1d2749
Ole Martin Bjørndalen <ombdalen@gmail.com>
2020-12-09 04:24:44 +0100

    Removed some unused imports.

--- canvas/gui.py
+++ canvas/gui.py
@@ -1,8 +1,6 @@
-import sys
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, GObject
-from . import audio
 from .timeline import Timeline
 from .transport import Transport
 
